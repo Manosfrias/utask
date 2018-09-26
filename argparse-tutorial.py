34bfc5ad45433dd864cbe7f4873b3d98092d5648
Manosfrias <a.gilamor@gmail.com>
2018-09-26 22:22:46 +0200

    Tutorial de argparse terminado

--- argparse-tutorial.py
+++ argparse-tutorial.py
@@ -1,12 +1,19 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("square", type=int, help="display a square of a given number")
-parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
+parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+group = parser.add_mutually_exclusive_group()
+
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+
+group.add_argument("-v", "--verbosity", action="store_true", help="increase output verbosity")
+group.add_argument("-q", "--quiet", action="store_true",  help="decrease output verbosity")
+
 args = parser.parse_args()
-answer = args.square**2
-if args.verbose >= 2:
-	print("the square of {} equals {}".format(args.square, answer))
-elif args.verbose >= 1:
-	print("{}^2 == {}".format(args.square, answer))
-else:
+answer = args.x**args.y
+
+if args.quiet:
 	print(answer)
+elif args.verbosity:
+	print("{} to the power {} equals {}".format(args.x, args.y, answer))
+else:
+	print("{}^{} == {}".format(args.x, args.y, answer))
