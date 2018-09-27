cc93ec872fb00d68737bafcf38c7272c26f3c44a
Ana Gil <agil@cpuras-iMac.local>
2018-09-27 17:55:35 +0200

    Crea el archivo en dónde voy a trabajar

---
+++ arg-uTask.py
@@ -0,0 +1,24 @@
+import argparse
+parser = argparse.ArgumentParser(description="Create task manager by terminal")
+
+parser.add_argument("name", help="The task name")
+
+parser.add_argument("-a", "--add", action="store_true", help="Add a new task")
+# parser.add_argument("-l", "--list", action="store_true", help="Add a new task")
+
+args = parser.parse_args()
+
+if args.add:
+	print(args.name)
+
+
+# class utask
+#   def __init__ (self, name)
+#     self._name = name
+
+#   def add(self, name) 
+#     self.name = name
+
+#   def list
+#     for utask in utaskList
+#       print(utask)
