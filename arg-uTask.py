7361d5aeb1b3f8b5337a5634d32e5255ddd9a279
Ana Gil <agil@cpuras-iMac.local>
2018-09-28 14:05:03 +0200

    El programa guarda el nombre de la tarea y lista tareas

--- arg-uTask.py
+++ arg-uTask.py
@@ -4,24 +4,27 @@ parser = argparse.ArgumentParser(description="Create task manager by terminal")
 parser.add_argument("name", help="The task name")
 
 parser.add_argument("-a", "--add", action="store_true", help="Add a new task")
-# parser.add_argument("-l", "--list", action="store_true", help="Add a new task")
+parser.add_argument("-l", "--list", action="store_true",  help="List all the tasks")
 
 args = parser.parse_args()
 
 if args.add:
-    tasksLibrary = open("task",'w')
-    tasksLibrary.write(args.name)
-    tasksLibrary.close()
-	print("Se ha añadido {} a la lista de tareas".format(args.name))
+   with open("task.txt",'a') as tasksLibrary:
+    tasksLibrary.write(args.name + '\n')
+    print("Se ha añadido {} a la lista de tareas".format(args.name))
+elif args.list:
+    with open("task.txt",'r', encoding='utf-8') as tasksLibrary:
+        for task in tasksLibrary:
+            print('{}'.format(task)) 
 
 
+# with open('test.txt', 'w') as a:
+#         a.write('theunixdisaster\t 05')
 # class utask
 #   def __init__ (self, name)
 #     self._name = name
-
 #   def add(self, name) 
 #     self.name = name
-
 #   def list
 #     for utask in utaskList
 #       print(utask)
