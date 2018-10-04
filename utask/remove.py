75dff36e71526f05f2b60707476f6acb6bbd6b7e
Ana Gil <agil@cpuras-iMac.local>
2018-10-04 17:54:06 +0200

    [WIP]

--- utask/remove.py
+++ utask/remove.py
@@ -1,11 +1,10 @@
 def remove(name):
     with open("task.txt",'w', encoding='utf-8') as tasksLibrary:
-        if name in tasksLibrary:
-            try:
-                tasksLibrary.pop(tasksLibrary.index(name))
-                print("Se ha eliminado {} de la lista de tareas".format(name))
-            except ValueError:
-                pass
-        else:
-            print("{} no existe en la lista de tareas".format(name))
+        task = tasksLibrary.readline()
+        for task in tasksLibrary:
+            if task != name:
+                print(task)
+                tasksLibrary.write(task)
+            else:
+                print('{} se ha eliminado de la lista'.format(task))
                 
