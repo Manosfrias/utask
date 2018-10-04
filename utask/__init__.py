b4bc529671eb7c11a80f421cb46e5d169e3e2b29
Ana Gil <agil@cpuras-iMac.local>
2018-10-04 15:49:03 +0200

    Quitándole el parámetro a la acción list

--- utask/__init__.py
+++ utask/__init__.py
@@ -2,6 +2,6 @@ import argparse
 parser = argparse.ArgumentParser(description="Create task manager by terminal")
 
 parser.add_argument("name", help="The task name")
+parser.add_argument("list", action="store_true",  help="List all the tasks")
 
 parser.add_argument("-a", "--add", action="store_true", help="Add a new task")
-parser.add_argument("-l", "--list", action="store_true",  help="List all the tasks")
