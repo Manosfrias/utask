8c7e7b371e887fea1516342d71616a23cf35e65c
Manosfrias <a.gilamor@gmail.com>
2018-10-12 11:44:38 +0200

    Cambiando el .txt por un .json

--- utask/add.py
+++ utask/add.py
@@ -1,4 +1,14 @@
+import json
 def add(name):
-    with open("task.txt",'a') as tasksLibrary:
-        tasksLibrary.write(name + '\n')
-        print("Se ha añadido {} a la lista de tareas".format(name))
+    task = {"tarea": []}
+    data_holder = task["tarea"]
+    counter = 0
+    while counter < 2:
+        data_holder.append({"nombre": name})
+        data_holder.append({"descripcion": name})
+        counter += 1
+    with open("tasks.json",'a') as tasksLibrary:
+        print(task)        
+        json.dump(task, tasksLibrary)
+        # tasksLibrary.write(name + '\n')
+    print("Se ha añadido {} a la lista de tareas".format(name))
