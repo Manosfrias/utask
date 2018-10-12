b861714713b6122a3c857da48fe88c23002a89f2
Manosfrias <a.gilamor@gmail.com>
2018-10-12 12:52:38 +0200

    [WIP] Añadiendo tareas codificadas en json

--- utask/add.py
+++ utask/add.py
@@ -1,14 +1,10 @@
 import json
 def add(name):
-    task = {"tarea": []}
-    data_holder = task["tarea"]
-    counter = 0
-    while counter < 2:
-        data_holder.append({"nombre": name})
-        data_holder.append({"descripcion": name})
-        counter += 1
-    with open("tasks.json",'a') as tasksLibrary:
-        print(task)        
+    task = {'tarea': {'nombre': name,'descripcion': name}}
+
+    with open("tasks.json",'a') as tasksLibrary:    
         json.dump(task, tasksLibrary)
         # tasksLibrary.write(name + '\n')
-    print("Se ha añadido {} a la lista de tareas".format(name))
+    with open("tasks.json",'r') as tasksLibrary: 
+        json.load(tasksLibrary)
+    # print("Se ha añadido {} a la lista de tareas".format(name))
